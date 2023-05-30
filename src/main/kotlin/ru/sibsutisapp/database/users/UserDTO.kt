c5ec94df4bec64619d4171e9d28759ba64203c8f
egork4 <egorbauer@yandex.ru>
2023-05-30 20:33:09 +0700

    Added val fio in class UserDTO

--- src/main/kotlin/ru/sibsutisapp/database/users/UserDTO.kt
+++ src/main/kotlin/ru/sibsutisapp/database/users/UserDTO.kt
@@ -1,8 +1,12 @@
 package ru.sibsutisapp.database.users
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 class UserDTO(
     val login:String,
     val password:String,
     val groupID:String,
     val role:String,
+    val fio:String,
 )
